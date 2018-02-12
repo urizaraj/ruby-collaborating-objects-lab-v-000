a4f1a53562a7e261611c6a07ffbe7a09c08dbc19
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 21:29:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,7 +19,7 @@ class Artist
 
   def self.find_or_create_by_name(name)
     result = @@all.find { |artist| artist.name == name }
-    puts result
+    
     if result
       return result
     else
