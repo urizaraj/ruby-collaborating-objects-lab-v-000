80088a9ae99c0c068dfc6283ae7d97a6ec93109d
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 21:26:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,8 @@ class Song
 
   def self.new_by_filename(file_name)
     artist, name, genre = file_name.split(' - ')
+    puts artist
+    puts name
 
     song = self.new(name)
     song.artist_name = artist
