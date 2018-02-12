d650fd30f1329572ee583c9d90287fd6e437117f
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 21:27:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,7 @@ class Song
 
   def self.new_by_filename(file_name)
     artist, name, genre = file_name.split(' - ')
-    puts artist
-    puts name
+    puts "[#{artist}][#{name}]"
 
     song = self.new(name)
     song.artist_name = artist
