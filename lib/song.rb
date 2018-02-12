009945d1243810d89e80ad757332e6c002b5110e
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 21:41:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   end
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(artist)
+    self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
   end
 end
