5f3a6f1ff825b0fa7efd0f357e076ef127402bdf
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 20:41:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,7 @@ class Song
 
   def self.new_by_filename(file_name)
     artist, name = file_name.scan(/([a-zA-Z ]+) - ([a-zA-Z ]+)/)[0]
-    song = self.new
-    song.name = name
+    song = self.new(name)
     song.artist = Artist.find_or_create_by_name(artist)
   end
 end
